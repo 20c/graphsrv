60fa38d9f16cfd3dd1bee1b49f8e8234fd4bc1f1
Stefan Pratter <stefan@20c.com>
2016-07-31 18:31:04 +0000

    ready for pip release

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
+from setuptools import find_packages, setup
 
-from setuptools import setup
+version = open('facsimile/VERSION').read().strip()
+requirements = open('facsimile/requirements.txt').read().split("\n")
+test_requirements = open('facsimile/requirements-test.txt').read().split("\n")
 
 setup(
     name='graphsrv',
-    version='0.1',
+    version=version,
     author='Twentieth Century',
     author_email='code@20c.com',
     description='serve embeddable graphs',
+    license='LICENSE.txt',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    packages=[
-      'graphsrv'
-    ],
-    install_requires=open("facsimile/requirements.txt").read().split("\n"),
+    packages = find_packages(),
     include_package_data=True,
+    url='https://github.com/20c/graphsrv',
+    download_url='https://github.com/20c/graphsrv/%s' % version,
+    install_requires=requirements,
+    test_requires=test_requirements,
     zip_safe=False
 )
