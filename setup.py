a981633c6f2a65631aad0d9c12be84540f12c33c
Stefan Pratter <stefan@20c.com>
2019-10-07 07:30:05 +0000

    move to ctl

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
-version = open('facsimile/VERSION').read().strip()
-requirements = open('facsimile/requirements.txt').read().split("\n")
-test_requirements = open('facsimile/requirements-test.txt').read().split("\n")
+version = open('Ctl/VERSION').read().strip()
+requirements = open('Ctl/requirements.txt').read().split("\n")
+test_requirements = open('Ctl/requirements-test.txt').read().split("\n")
 
 setup(
     name='graphsrv',
